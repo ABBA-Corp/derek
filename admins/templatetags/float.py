4ea399e06d1c12479a2cfd2a6006d7bf72c4c39e
MSD <dadamuhames@gmail.com>
2023-02-19 15:17:17 +0500

    fix tamplate tag float

--- admins/templatetags/float.py
+++ admins/templatetags/float.py
@@ -3,7 +3,4 @@ from django.template.defaulttags import register
 
 @register.filter
 def to_float(val):
-    if ',' in str(val):
-        return str(val).replace(',', '.')
-    else:
-        return val
+    return str(val)
