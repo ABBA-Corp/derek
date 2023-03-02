e7d0eb1dac13238a0997f7a8706d4e1354d878b1
MSD <dadamuhames@gmail.com>
2023-03-02 22:07:07 +0500

    fix admin

--- derek/settings.py
+++ derek/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'derek.middleware.LoginRequiredMiddleware',
+    #'derek.middleware.LoginRequiredMiddleware',
 ]
 
 ROOT_URLCONF = 'derek.urls'
