9571db53512fede26967eb3a834d3864fd5affcb
MSD <dadamuhames@gmail.com>
2023-03-06 17:35:30 +0500

    Fix cors

--- derek/settings.py
+++ derek/settings.py
@@ -183,7 +183,7 @@ REST_FRAMEWORK = {
 
 # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000", 'https://derek-ndc.vercel.app', "https://derek.itlink.uz"
+    "http://localhost:3000", 'https://derek-ndc.vercel.app', "https://derek.itlink.uz", 'https://derekdental.com'
 ]
 
 
